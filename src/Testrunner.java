ee4b68805341bb3e53ca8ecbb82ff85542019431
Tejas Menon <tejas.menon@hotmail.com>
2019-02-27 18:11:29 -0800

    TestRunner class performs all analysis

---
+++ src/Testrunner.java
@@ -0,0 +1,9 @@
+public class Testrunner {
+    private Testdata data;
+
+    public void run() {
+        String [] input = {"totalrand","totaldup","almostsortdup","sortdup","revsortdup",
+                            "almostrevsortdup","randup","almostsortuniq","revsortuniq","almostrevsortuniq","randuniq"};
+        int size = 1;
+    }
+}
